9558392e0a1503c9a970cfe4ab02d0c8f9490e72
Alex Williams <alexanderwilliams@Alexanders-MacBook-Pro-3.local>
2020-05-17 14:16:44 -0400

    added profile image, organized utilMongoose, added student routes and model

--- server/models/alumniSchema.js
+++ server/models/alumniSchema.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const alumniSchema = new Schema(
   {
+    imageURL: {type: String, default:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'},
     name: {type: String, required: true},
     email: {type: String, reguired: true},
     gradYear: {type: Number, required: true},
