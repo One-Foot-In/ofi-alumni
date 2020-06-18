bd0ffafcce9c06773dc3dff8ca201745756965c3
reshadbinharun <reshadbinharun@gmail.com>
2020-06-17 23:22:40 -0500

    integrated emails for new and confirmed requests

--- server/routes/helpers/emailHelpers.js
+++ server/routes/helpers/emailHelpers.js
@@ -1,7 +1,8 @@
 const sg = require('@sendgrid/mail');
 require('dotenv').config();
 
-const BACKEND = process.env.BACKEND || 'https://localhost:5000'
+const BACKEND = process.env.BACKEND || 'http://localhost:5000'
+const APP = process.env.APP || 'http://localhost:3000'
 var htmlBuilder = require('./emailBodyBuilder').buildBody
 
 // Comment out actual sendgrid call to avoid hitting free quota during testing
@@ -54,6 +55,49 @@ const sendStudentVerificationEmail = async (to, token, schoolName) => {
   await sg.send(emailObject, true)
 }
 
+const sendNewRequestEmail = async (to) => {
+  let emailObject = createPersonalization(
+    to,
+    'New Request!',
+    htmlBuilder(
+      'You have a new request!',
+      `You have a mentee who needs your help! Please login to app to view your new requests!`,
+      'Go To App',
+      APP
+    )
+  )
+  // console.log("Sending email with", emailObject)
+  await sg.send(emailObject, true)
+}
+
+const sendRequestConfirmedEmail = async (menteeEmail, menteeName, menteeTime, mentorEmail, mentorName, mentorTime, topic) => {
+  let emailObjectForMentee = createPersonalization(
+    menteeEmail,
+    'You\'re request has been confirmed!',
+    htmlBuilder(
+      'You\'re all set!',
+      `You're set to take a call with ${mentorName} discussing ${topic}. The call is set to take place ${menteeTime}. We <b> strongly recommend </b> setting up a reminder so you don't forget the call! You can go into the app, and join the video call.`,
+      'Go To App',
+      APP
+    )
+  )
+  let emailObjectForMentor = createPersonalization(
+    mentorEmail,
+    'You\'ve confirmed a request!',
+    htmlBuilder(
+      'Thank you for confirming your request!',
+      `You're set to take a call with ${menteeName} discussing ${topic}. The call is set to take place ${mentorTime}. We <b> strongly recommend </b> setting up a reminder so you don't forget the call! You can go into the app, and join the video call.`,
+      'Go To App',
+      APP
+    )
+  )
+  // console.log("Sending email with", emailObject)
+  await sg.send(emailObjectForMentee, true)
+  await sg.send(emailObjectForMentor, true)
+}
+
 exports.sendTestEmail = sendTestEmail
 exports.sendAlumniVerificationEmail = sendAlumniVerificationEmail
 exports.sendStudentVerificationEmail = sendStudentVerificationEmail
+exports.sendNewRequestEmail = sendNewRequestEmail
+exports.sendRequestConfirmedEmail = sendRequestConfirmedEmail
