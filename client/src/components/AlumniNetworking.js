342bebbff8683fae4d867c6844b6c0da53ba0de6
Alexander Williams <alex@Alexanders-MacBook-Pro.local>
2020-07-03 19:38:52 -0400

    finished conversation modal format, fixed safari bug

--- client/src/components/AlumniNetworking.js
+++ client/src/components/AlumniNetworking.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-import { Card, Image, Grid, Label, icon } from 'semantic-ui-react'
+import { Card, Image, Grid, Label, Modal, Button, Feed, Divider, Form, Icon} from 'semantic-ui-react'
 import { makeCall } from '../apis';
 
+/*
+ * DETAILS: Parent component, displays available conversations and 
+ *          if the most recent message has been seen
+ * PROPS: userDetails
+ */ 
 export default class AlumniNetworking extends Component {
     constructor(props){
         super(props)
         this.state={
             conversations: [],
             timeOffsetHours: null,
-            display: []
+            display: [],
+            activeConversation: null,
+            conversationModalOpen: false
         }
+        this.closeConversationModal = this.closeConversationModal.bind(this)
+        this.openConversation = this.openConversation.bind(this)
     }
 
     async componentWillMount() {
@@ -23,7 +32,7 @@ export default class AlumniNetworking extends Component {
     }
 
     getConversations() {
-        return makeCall({}, '/conversations/get/' + this.props.userDetails._id, 'get')
+        return makeCall({}, '/conversations/all/' + this.props.userDetails._id, 'get')
     }
 
     createDisplay(conversations) {
@@ -34,9 +43,44 @@ export default class AlumniNetworking extends Component {
         this.setState({display: display})
     }
 
+    async openConversation(e) {
+        let conversationTarget = e.currentTarget.getAttribute('conversationid')
+        let conversation = await this.fetchConversation(conversationTarget)
+        this.setState({
+                activeConversation: conversation.conversation,
+                conversationModalOpen: true
+            })
+    }
+
+    fetchConversation(conversationTarget) {
+        return (makeCall({id: conversationTarget, timezone: this.state.timeOffsetHours},
+            '/conversations/one/' + this.props.userDetails._id, 'patch'))
+    }
+
+    async closeConversationModal() {
+        let conversations = await this.getConversations()
+        this.setState({
+            conversations: conversations.conversations,
+            conversationModalOpen: false, 
+            activeConversation: null
+        })
+        this.createDisplay(conversations.conversations)
+    }
+
     constructConversation(conversation) {
         let userIndex = conversation.alumni.findIndex(item => item._id === this.props.userDetails._id);
         let recipientIndex = (userIndex + 1) % 2;
+        let formattedMessage = conversation.messages[0].message.split('\n').map((message, i) => {
+            if (i === 0) {
+                return(
+                    <p style={{'wordWrap': 'break-word', 'hyphens': 'auto', 'width': '50vw'}} key={i}>
+                        {message}
+                    </p>
+                )
+            } else {
+                return
+            }
+        })
         return(
             <Grid key={conversation._id} columns={'equal'}>
                 <Grid.Row columns={2}>
@@ -49,7 +93,7 @@ export default class AlumniNetworking extends Component {
                         />
                     </Grid.Column>
                     <Grid.Column>
-                        <Card fluid>
+                        <Card fluid onClick={this.openConversation.bind(this)} conversationid={conversation._id}>
                             <Card.Content>
                             {!conversation.seen[userIndex] && 
                                 <Label color='teal' corner='right' icon='envelope' />
@@ -58,8 +102,7 @@ export default class AlumniNetworking extends Component {
                                     Conversation with {conversation.alumni[recipientIndex].name}
                                 </Card.Header>
                                 <Card.Meta>{conversation.timeFromMessage}</Card.Meta>
-                                
-                                <Card.Description>Most recent message: {conversation.messages[0].message}</Card.Description>
+                                <Card.Description>Most recent message: <br/>{formattedMessage}</Card.Description>
                                 
                             </Card.Content>
                         </Card>
@@ -72,8 +115,125 @@ export default class AlumniNetworking extends Component {
     render() {
         return (
             <>
+            {this.state.activeConversation &&
+                <Conversation 
+                    conversation={this.state.activeConversation}
+                    modalOpen={this.state.conversationModalOpen}
+                    closeModal={this.closeConversationModal}
+                    userDetails={this.props.userDetails}
+                />
+            }
             {this.state.display}
             </>
+        )
+    }
+}
+
+class Conversation extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            conversation: null,
+            display: [],
+            message: ''
+        }
+        this.handleValueChange = this.handleValueChange.bind(this)
+    }
+    componentWillMount() {
+        this.createDisplay(this.props.conversation)
+        this.setState({conversation: this.props.conversation})
+    }
+
+    createDisplay(conversation) {
+        let display = []
+        for (let message of conversation.messages) {
+            display.unshift(this.displayMessage(message, conversation.alumni, conversation.messages[0]))
+        }
+        this.setState({display: display})
+    }
+
+    handleValueChange(e, {name, value}) {
+        e.preventDefault();
+        this.setState({
+            [name]: value
+        })
+    }
+
+    displayMessage(message, alumni) {
+        let userIndex = alumni.findIndex(item => item._id === this.props.userDetails._id);
+        let recipientIndex = (userIndex + 1) % 2;
+        let formattedMessage = message.message.split('\n').map((message, i) => {
+            return(
+                <p style={{'wordWrap': 'break-word', 'hyphens': 'auto', 'width': '50vw'}} key={i}>
+                    {message}
+                </p>
+                )
+        })
+        if (message.senderId[0] === this.props.userDetails._id) {
+            return (
+                <>
+                <Divider/>
+                <Feed.Event>
+                    <Feed.Label><Image src={alumni[userIndex].imageURL}/></Feed.Label>
+                    <Feed.Content>
+                        <Feed.Summary>{alumni[userIndex].name} <Feed.Date>{message.dateString}</Feed.Date></Feed.Summary>
+                        {formattedMessage}
+                    </Feed.Content>
+                </Feed.Event>
+                </>
+           )
+       } else {
+            return (
+                <>
+                <Divider/>
+                <Feed.Event style={{'overflowWrap': 'break-word', 'hyphens': 'auto'}}>
+                    <Feed.Label><Image src={alumni[recipientIndex].imageURL}/></Feed.Label>
+                    <Feed.Content>
+                        <Feed.Summary>{alumni[recipientIndex].name} <Feed.Date>{message.dateString}</Feed.Date></Feed.Summary>
+                        {formattedMessage}
+                    </Feed.Content>
+                </Feed.Event>
+                </>
+            )
+       }
+    }
+
+    render() {
+        let userIndex = this.state.conversation.alumni.findIndex(item => item._id === this.props.userDetails._id);
+        let recipientIndex = (userIndex + 1) % 2;
+        return (
+            <Modal 
+                open={this.props.modalOpen} 
+                closeIcon 
+                onClose={this.props.closeModal}
+            >
+                <Modal.Header>Conversation with {this.state.conversation.alumni[recipientIndex].name}</Modal.Header>
+                <Modal.Content scrolling style={{'display': 'flex', 'flexDirection': 'column-reverse', 'maxHeight': '50vh'}}>
+                    <Feed>
+                        {this.state.display}
+                    </Feed>
+                </Modal.Content>
+                <Modal.Content>
+                    <Grid>
+                        <Grid.Row columns={'equal'} verticalAlign='middle'>
+                            <Grid.Column width={12}>
+                                <Form>
+                                    <Form.TextArea
+                                        rows={2}
+                                        label={'Send a message to ' + this.state.conversation.alumni[recipientIndex].name + ':'}
+                                        onChange={this.handleValueChange}
+                                        value={this.state.message}
+                                        name='message'
+                                    /> 
+                                </Form>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Button inverted floated='left'><Icon size='large' color='blue' name='paper plane'/></Button>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Modal.Content>
+            </Modal>
         )
     }
 }
