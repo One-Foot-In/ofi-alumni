291ec8114b068a8bac222b66590e1937ecf00f92
reshadbinharun <reshadbinharun@gmail.com>
2020-09-13 15:09:36 -0400

    use multer for initial image upload

--- server/routes/image.js
+++ server/routes/image.js
@@ -5,7 +5,6 @@ var AWS = require('aws-sdk');
 var fs = require('fs');
 var fileType = require('file-type');
 var bluebird = require('bluebird');
-var multiparty = require('multiparty');
 require('dotenv').config();
 var adminSchema = require('../models/adminSchema');
 var schoolSchema = require('../models/schoolSchema');
@@ -14,6 +13,7 @@ var studentSchema = require('../models/studentSchema');
 require('mongoose').Promise = global.Promise
 var bcrypt = require('bcrypt');
 var moment = require('moment');
+var multer = require('multer'), multerS3 = require('multer-s3');
 
 const HASH_COST = 4;
 
@@ -29,25 +29,44 @@ AWS.config.setPromisesDependency(bluebird);
 // create S3 instance
 const s3 = new AWS.S3();
 
-// abstracts function to upload a file returning a promise
-const uploadFile = async (buffer, name, type) => {
-const params = {
-    ACL: 'public-read',
-    Body: buffer,
-    Bucket: process.env.S3_BUCKET,
-    ContentType: type.mime,
-    Key: `${name}.${type.ext}`
-};
-return await s3.upload(params).promise();
-};
+var multerUpload = multer({
+    limits: {
+        fileSize: 20 * 1048576 // 20MB max fileSize, will need to reduce this and apply frontend check for max file size
+    },
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.S3_BUCKET,
+        acl: 'public-read',
+        cacheControl: 'max-age=31536000',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: async (req, file, cb) => {
+            let email = req.body.email;
+            let emailHash = await bcrypt.hash(email, HASH_COST);
+            let fileName = `${emailHash.replace('/', '')}-${moment().format('MM-DD-YYYY')}`;
+            cb(null, fileName);
+        }
+    })
+});
+
+// TODO: this potentially leaves S3 bucket being polluted with images, but these images will never be associated to any user
+router.post('/add', multerUpload.single('imageFile'), async (req, res, next) => {
+    try {
+        let imageLocation = req.file.location;
+        res.status(200).send({
+            success: true,
+            imageUrl: imageLocation
+        });
+    } catch (e) {
+        console.log("Error: image#addImage", e);
+        res.status(500).send({'error' : e});
+    }
+});
 
 async function isAdmin(id) {
     let admin = await adminSchema.findById(id)
     let alumni = await alumniSchema.findById(id).populate('user')
     return (admin !== null || (alumni && alumni.user.role.includes('ADMIN')))
 }
-
-//TODO: Fix alumni, student, and school routes
 
 router.post('/alumni/:alumniId', passport.authenticate('jwt', {session: false}), async (req, res, next) => {
     try {
@@ -126,32 +145,6 @@ router.post('/school/', passport.authenticate('jwt', {session: false}), async (r
         }) 
     } catch (e) {
         console.log("Error: image#school", e);
-        res.status(500).send({'error' : e});
-    }
-});
-
-// TODO: this potentially leaves S3 bucket being polluted with images, but these images will never be associated to any user
-router.post('/add', async (req, res, next) => {
-    try {
-        const form = new multiparty.Form();
-        form.parse(req, async (error, fields, files) => {
-            if (!error) {
-                const email = fields.email[0]
-                const emailHash = await bcrypt.hash(email, HASH_COST);
-                const path = files.imageFile[0].path;
-                const buffer = fs.readFileSync(path);
-                const type = await fileType.fromBuffer(buffer);
-                const fileName = `${emailHash}-${moment().format('MM-DD-YYYY')}`;
-                const data = await uploadFile(buffer, fileName, type);
-                let imageLocation = data.Location;
-                res.status(200).send({
-                    success: true,
-                    imageUrl: imageLocation
-                })
-            }
-        })
-    } catch (e) {
-        console.log("Error: image#addImage", e);
         res.status(500).send({'error' : e});
     }
 });
