21a7dc63e3384a5c40ae74fd96aff44e5887d914
Scott Bettigole <scott.bettigole@tufts.edu>
2021-01-17 15:44:12 -0500

    Starter part of script for doing deploys

--- server/migrations/20210117053804-add-footy-points.js
+++ server/migrations/20210117053804-add-footy-points.js
@@ -2,17 +2,10 @@ module.exports = {
   async up(db, client) {
     await db.collection('alumnis').updateMany({}, {$set: {footyPoints: 0}});
     await db.collection('students').updateMany({}, {$set: {footyPoints: 0}});
-    // TODO write your migration here.
-    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
   },
 
   async down(db, client) {
     await db.collection('alumnis').updateMany({}, {$unset: {footyPoints: null}});
     await db.collection('students').updateMany({}, {$unset: {footyPoints: null}});
-    // TODO write the statements to rollback your migration (if possible)
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
   }
 };
