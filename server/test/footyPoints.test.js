282cc4a1255d136a285401518860c5c06b3f51ec
Scott Bettigole <scott.bettigole@tufts.edu>
2021-01-25 23:02:04 -0500

    Better test

--- server/test/footyPoints.test.js
+++ server/test/footyPoints.test.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const createServer = require("./testingTools")
+var alumniSchema = require('../models/alumniSchema');
+
 // const { app } = require('../app');
 
 // const createServer = require("./server") // new
@@ -17,9 +19,11 @@ const createServer = require("./testingTools")
 
 const request = require('supertest');
 
-beforeAll(() => {
+beforeAll(async () => {
     process.env.NODE_ENV = 'test';
     mongoose.connect("mongodb://localhost:27017/test-test", { useNewUrlParser: true });
+    const seed = await request(app).get('/util/seed/');
+    console.log(seed.status);
 });
 
 afterAll(async () => {
@@ -33,10 +37,22 @@ afterAll(async () => {
 
 const app = createServer();
 
-test('test', async () => {
-    const seed = await request(app).get('/util/seed/');
-    console.log(seed.status);
-    const res = await request(app).get('/alumni/one/1');
-    console.log(res.status);
-    console.log(res.body);
-});
+// test('test', async () => {
+//     const res = await request(app).get('/alumni/one/1');
+//     console.log(res.status);
+//     console.log(res.body);
+// });
+
+test('better test', async () => {
+    user = await alumniSchema.findOne()
+    console.log(user)
+    console.log(user._id);
+    const res = await request(app).post(`/alumni/opportunity/${user._id}`).send({
+        description: 'asdf',
+        deadline: '10/12/2021',
+        link: 'example.com'
+    });
+    expect(res.status).toEqual(200);
+    refresh_user = await alumniSchema.findOne({_id: user._id});
+    expect(refresh_user.footyPoints).toEqual(8);
+})
