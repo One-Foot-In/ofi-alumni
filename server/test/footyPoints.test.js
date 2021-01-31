152f13541eec256c7927b23c76523bdf75993102
Scott Bettigole <scott.bettigole@tufts.edu>
2021-01-31 18:43:54 -0500

    Cleanup, make tests work nicely. Didn't fix auth issues

--- server/test/footyPoints.test.js
+++ server/test/footyPoints.test.js
@@ -1,31 +1,34 @@
-const express = require("express")
 const mongoose = require("mongoose")
 const createServer = require("./testingTools")
 var alumniSchema = require('../models/alumniSchema');
-
-// const { app } = require('../app');
-
-// const createServer = require("./server") // new
-
-// mongoose
-// 	.connect("mongodb://localhost:27017/acmedb", { useNewUrlParser: true })
-// 	.then(() => {
-// 		const app = createServer() // new
-// 		app.listen(5000, () => {
-// 			console.log("Server has started!")
-// 		})
-// 	})
-
-
 const request = require('supertest');
 
+
+const app = createServer();
+
+/**
+ * This runs before all tests in this particular file. I believe
+ * this will needed to be included in every test file to get them
+ * to work right.
+ * I'd bet there's a way to get this to run once per test suite
+ * instead of once per file, but I haven't looked into it yet.
+ */
 beforeAll(async () => {
     process.env.NODE_ENV = 'test';
     mongoose.connect("mongodb://localhost:27017/test-test", { useNewUrlParser: true });
-    const seed = await request(app).get('/util/seed/');
-    console.log(seed.status);
+    await request(app).get('/util/seed/');
 });
 
+
+/**
+ * Same thing as the beforeAll function, needs to be included on
+ * all test files where you're hitting API endponts.
+ * Just cleans up the tests after they've run.
+ * 
+ * The database might be better off getting reset after each test,
+ * rather than after all have run, to help each test run independently
+ * and be unaffected by surrounding tests (idempotent)
+ */
 afterAll(async () => {
     const collections = Object.keys(mongoose.connection.collections);
     for (const collectionName of collections) {
@@ -35,24 +38,20 @@ afterAll(async () => {
     mongoose.connection.close();
 });
 
-const app = createServer();
-
-// test('test', async () => {
-//     const res = await request(app).get('/alumni/one/1');
-//     console.log(res.status);
-//     console.log(res.body);
-// });
-
-test('better test', async () => {
-    user = await alumniSchema.findOne()
-    console.log(user)
-    console.log(user._id);
-    const res = await request(app).post(`/alumni/opportunity/${user._id}`).send({
-        description: 'asdf',
-        deadline: '10/12/2021',
-        link: 'example.com'
-    });
+test('8 footy points added for new opportunity post', async () => {
+    user = await alumniSchema.findOne();
+    // Need to extract the user ID for being able to make calls to the 
+    // endpoints regarding the specific user (alumni, in this case)
+    const userID = user._id;
+    const res = await request(app).post(`/alumni/opportunity/${userID}`)
+        .send({
+            description: 'asdf',
+            deadline: '10/12/2021',
+            link: 'example.com'
+        });
     expect(res.status).toEqual(200);
-    refresh_user = await alumniSchema.findOne({_id: user._id});
+    // Before checking if the footy points updated, have to get the
+    // updated user info by pulling from the db again
+    refresh_user = await alumniSchema.findOne({_id: userID});
     expect(refresh_user.footyPoints).toEqual(8);
 })
